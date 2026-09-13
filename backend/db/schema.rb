adf456321a4d533e22b3d89551f1f0ff287f7f46
David Behling <david_behling@hotmail.com>
2026-09-13 14:53:46 -0300

    feat: criado modelos city e state, criado seed

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2026_09_13_145742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "population"
+    t.bigint "state_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.integer "population"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "cities", "states"
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+pr = State.create({ name: 'Paraná', population: '11516840' })
+sc = State.create({ name: 'Santa Catarina', population: '7609601' })
+rs = State.create({ name: 'Rio Grande do Sul', population: '10880506' })
+
+City.create([
+  { name: 'Curitiba', population: '3894896', state: pr },
+  { name: 'Guarapuava', population: '182093', state: pr },
+  { name: 'Londrina', population: '555937', state: pr },
+  { name: 'Maringá', population: '409657', state: pr },
+  { name: 'Foz do Iguaçu', population: '285415', state: pr },
+  { name: 'Florianópolis', population: '1189947', state: sc },
+  { name: 'Blumenau', population: '363340', state: sc },
+  { name: 'Joinville', population: '616323', state: sc },
+  { name: 'Balneário Camboriú', population: '139155', state: sc },
+  { name: 'Porto Alegre', population: '1332570', state: rs },
+  { name: 'Pelotas', population: '325689', state: rs },
+  { name: 'Gramado', population: '44643', state: rs },
+  { name: 'Bento Gonçalves', population: '123151', state: rs }
+])
