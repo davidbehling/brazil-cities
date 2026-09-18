e166f02f233d31b3cc756592a83879659ea2b5c5
David Behling <david_behling@hotmail.com>
2026-09-18 15:48:05 -0300

    teste: adiciona testes dos models de cidades e estados

--- backend/test/models/city_test.rb
+++ backend/test/models/city_test.rb
@@ -1,7 +1,68 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should belong to a state' do
+    city = cities(:one)
+
+    assert_respond_to city, :state
+    assert_equal states(:one), city.state
+  end
+
+  test 'should be valid with a name' do
+    city = City.new(
+      name: 'Joinville',
+      population: 600_000,
+      state: states(:one)
+    )
+
+    assert city.valid?
+  end
+
+  test 'should not be valid without a name' do
+    city = City.new(
+      population: 600_000,
+      state: states(:one)
+    )
+
+    assert_not city.valid?
+    assert_includes city.errors[:name], "can't be blank"
+  end
+
+  test 'search_by_name should find city by prefix' do
+    city = City.create!(
+      name: 'Joinville',
+      population: 600_000,
+      state: states(:one)
+    )
+
+    results = City.search_by_name('Join')
+
+    assert_includes results, city
+  end
+
+  test 'search_by_name should find city by similar name' do
+    city = City.create!(
+      name: 'Joinville',
+      population: 600_000,
+      state: states(:one)
+    )
+
+    results = City.search_by_name('Joinvile')
+
+    assert_includes results, city
+  end
+
+  test 'search_by_state_name should find cities by state name' do
+    state = states(:one)
+
+    city = City.create!(
+      name: 'Joinville',
+      population: 600_000,
+      state: state
+    )
+
+    results = City.search_by_state_name(state.name)
+
+    assert_includes results, city
+  end
 end
--- backend/test/models/state_test.rb
+++ backend/test/models/state_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid with a name' do
+    state = State.new(
+      name: 'Santa Catarina',
+      population: 600_000
+    )
+
+    assert state.valid?
+  end
+
+  test 'should not be valid without a name' do
+    state = State.new(
+      population: 600_000
+    )
+
+    assert_not state.valid?
+    assert_includes state.errors[:name], "can't be blank"
+  end
 end
